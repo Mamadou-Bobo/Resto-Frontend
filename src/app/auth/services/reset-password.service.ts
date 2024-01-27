89431a8a6e7704be3f0894c51c3550de8acc26f6
unknown <diallomamadoubobo999@gmail.com>
2024-01-27 13:03:25 +0000

    change the return type

--- src/app/auth/services/reset-password.service.ts
+++ src/app/auth/services/reset-password.service.ts
@@ -14,6 +14,6 @@ export class ResetPasswordService {
     constructor(private httpClient: HttpClient) {}
 
     public sendResetPasswordCode(email: Email): Observable<Object> {
-        return this.httpClient.post(this.API_URL + "/password/reset/code", email);
+        return this.httpClient.post<Object>(this.API_URL + "/password/reset/code", email);
     }
 }
