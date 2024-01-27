3ac48607fa182043e526bb59b4eb138d1578a486
unknown <diallomamadoubobo999@gmail.com>
2024-01-27 12:59:19 +0000

    fix condition inside send code function

--- src/app/auth/components/reset-password/reset-password.component.ts
+++ src/app/auth/components/reset-password/reset-password.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Email } from '../../../shared/models/Email';
 import { ResetPasswordService } from '../../services/reset-password.service';
 import { Subject, takeUntil } from 'rxjs';
+import { SnackBarService } from '../../../shared/services/snackbar.service';
+import { SnackBar } from '../../../shared/models/SnackBar';
 
 @Component({
   selector: 'app-reset-password',
@@ -10,7 +12,7 @@ import { Subject, takeUntil } from 'rxjs';
   styleUrl: './reset-password.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ResetPasswordComponent { 
+export class ResetPasswordComponent implements OnDestroy { 
 
   email!: Email;
 
@@ -21,23 +23,42 @@ export class ResetPasswordComponent {
     Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
   ]);
 
-  constructor(private resetPasswordService: ResetPasswordService) {}
+  constructor(private resetPasswordService: ResetPasswordService,
+              private snackBarService: SnackBarService) {}
+
+  ngOnDestroy(): void {
+    this.destroy$.next(false);
+    this.destroy$.unsubscribe();
+  }
 
   protected sendCode(): void {
+    let snackbar: SnackBar;
+
     if(this.resetPasswordForm.valid) {
 
       this.email = this.resetPasswordForm.value;
 
-      console.log(this.email);
-
-      /*this.resetPasswordService.sendResetPasswordCode(this.email).pipe(takeUntil(this.destroy$)).subscribe({
-        next(data) {
+      this.resetPasswordService.sendResetPasswordCode(this.email).pipe(takeUntil(this.destroy$)).subscribe({
+        next: (data) => {
           console.log(data);
         },
-        error(err) {
+        error: (err) => {
           console.log(err);
         },
-      });*/
+      });
+    } else if(this.resetPasswordForm.hasError('pattern')) {
+      snackbar = this.setSnackBar('Please enter a valid email','snackbar-err');
+      this.snackBarService.openSnackBar(snackbar);
+    } else {
+      snackbar = this.setSnackBar('Please fill all fields','snackbar-err');
+      this.snackBarService.openSnackBar(snackbar);
+    }
+  }
+
+  private setSnackBar(message: string, className: string): SnackBar {
+    return {
+      message: message,
+      className: className
     }
   }
 }
