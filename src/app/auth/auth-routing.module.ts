163a7bca2ae12ebbd993cdd88dba112094667936
unknown <diallomamadoubobo999@gmail.com>
2024-01-29 13:19:07 +0000

    add route title

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,19 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { VerifyAccountComponent } from './components/verify-account/verify-account.component';
 import { LoginComponent } from './components/login/login.component';
 import { UpdatePasswordComponent } from './components/update-password/update-password.component';
+import { Constant } from '../shared/utils/Constant';
 
 const authRoutes: Routes = [
     {
         path: 'login',
-        component: LoginComponent
+        component: LoginComponent,
+        title: Constant.APP_TITLE + 'Login'
     },
     {
         path: 'verify-account',
-        component: VerifyAccountComponent
+        component: VerifyAccountComponent,
+        title: Constant.APP_TITLE + 'Verify Account'
     },
     {
         path: 'reset-password',
-        component: UpdatePasswordComponent
+        component: UpdatePasswordComponent,
+        title: Constant.APP_TITLE + 'Reset Password'
     }
 ];
 
--- src/app/shared/utils/Constant.ts
+++ src/app/shared/utils/Constant.ts
@@ -2,4 +2,6 @@ export class Constant {
     static readonly SYS_ADMIN: string = "ROLE_SYS_ADMIN";
     static readonly COOK: string = "ROLE_COOK";
     static readonly WAITER: string = "WAITER";
+
+    static readonly APP_TITLE: string = "Resto - ";
 }
